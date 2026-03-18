f32ed947097cb5f91dad7fde3266539ac0107341
Mondo Daniel <mondodaniel7@gmail.com>
2026-03-18 13:05:03 +0100

    implementation de la classe Note avec validation de l'intervalle [0 - 20]

--- src/core/actors/Note.java
+++ src/core/actors/Note.java
@@ -1,18 +1,40 @@
 package core.actors;
-import core.exceptions.NoteInvalidException;
+import core.exceptions.NoteInvalideException;
 
+/**
+ * represente une evaluation obtenue par un etudiant
+ * <p>
+ * une note est composé d'une valeur numerique et d'un coefficient
+ * le systeme garantit qu'une note est toujours comprise dans l'intervalle de 0 a 20
+ * </p>
+ * 
+ * @author Mondo Daniel
+ * @version 1.0
+ */
 public class Note {
+
+    /** la valeur numerique de la note (entre 0 et 20) */
     private double valeur;
     private double coefficient;
 
-    public Note(double valeur, double coefficient) throws NoteInvalidException {
+    /**
+     * crée une nouvelle note avec validation de la valeur
+     * 
+     * @param valeur la note obtenue 
+     * @param coefficient  le poids de la note
+     * @param NoteInvalideException si la valeur est <0 ou >20
+     */
+public Note(double valeur, double coefficient) throws NoteInvalideException {
         if (valeur < 0 || valeur > 20) {
-            throw new NoteInvalidException("La note doit... 0 et 20");
+            throw new NoteInvalideException("Erreur de saisie : La note (" + valeur + ") doit etre comprise entre 0 et 20");
         }
         this.valeur = valeur;
         this.coefficient = coefficient;
     }
 
+    /** @return la valeur de la note */
     public double getValeur () {return valeur;}
+
+    /** @return le coefficient de la note */
     public double getCoefficient() {return coefficient;}
 }
