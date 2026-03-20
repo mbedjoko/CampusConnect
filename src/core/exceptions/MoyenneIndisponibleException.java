6fe98c615e63c2cdac407b0df691774913892fe5
Mondo Daniel <mondodaniel7@gmail.com>
2026-03-20 12:22:34 +0100

    Refactor(Exceptions): harmonisation et ajout de la javadoc

--- src/core/exceptions/MoyenneIndisponibleException.java
+++ src/core/exceptions/MoyenneIndisponibleException.java
@@ -1,29 +1,29 @@
 package core.exceptions;
 
-/**
- * l'exception intervient lorsqu'un etudiant essaie de calculer la moyenne
- * alors qu'aucune note n'est enregistre dans la liste
- * <p>
- * ca permet d'eviter les erreurs de calculs (division par 0)
- * </p>
- * 
- * @author Mondo Daniel
- * @version 1.0
- */
+    /**
+     * l'exception intervient lorsqu'un etudiant essaie de calculer la moyenne
+     * alors qu'aucune note n'est enregistre dans la liste
+     * <p>
+     * ca permet d'eviter les erreurs de calculs (division par 0)
+     * </p>
+     * 
+     * @author Mondo Daniel
+     * @version 1.0
+     */
 
 public class MoyenneIndisponibleException extends Exception {
 
     /**
      * identifiant de version pour la serialisation 
      */
-    private static final long serialVersionUID = 1L;
+private static final long serialVersionUID = 1L;
 
     /**
      * construit une nouvelle exception avec un message d'erreur specifique
      * 
-     * @param message le message expliquant la cause de lexception
+     * @param message le message expliquant la cause de l'exception
      */
-    public MoyenneIndisponibleException(String message) {
+public MoyenneIndisponibleException(String message) {
         super(message);
     }
 }
