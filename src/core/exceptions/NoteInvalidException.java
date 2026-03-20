1ced3f5e47cd1a8bb5bd8acb731853f9198015a0
Mondo Daniel <mondodaniel7@gmail.com>
2026-03-20 12:48:56 +0100

    Refactor(1 exception): suppression d'un ancien fichier

--- src/core/exceptions/NoteInvalidException.java
+++
@@ -1,8 +0,0 @@
-package core.exceptions;
-
-//si une note est [<0 ou >20]
-public class NoteInvalidException extends Exception {
-    public NoteInvalidException(String message) {
-        super(message);
-    }
-}
